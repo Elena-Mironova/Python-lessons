b1bc3c0fb10bb2aad81b71ae4b06d407c7f6a45b
Elena-Mironova <mironova_elenag@mail.ru>
2020-01-30 14:31:34 +0300

    Урок 2

--- Lesson 1.7. Инструкции break и continue, while-else.py
+++ Lesson 1.7. Инструкции break и continue, while-else.py
@@ -1,10 +1,30 @@
-#break
-name = None
-while True:
-      name = input('Кто создатель python? ')
-      if name == 'Гвидо':
-          break
-      print('НЕ верно')
+# #break
+# name = None
+# while True:
+#       name = input('Кто создатель python? ')
+#       if name == 'Гвидо':
+#           break
+#       print('НЕ верно')
+# print('Верно')
 
-print('Верно')
-# continue
+# # continue
+# number = 0
+# n = int(input('Введите n: '))
+# while number <= n:
+#     if number % 2 == 0:
+#         number += 1
+#         continue
+#     print(number)
+#     number += 1
+
+#while-else
+number = 0
+
+while number <= 100:
+    print(number)
+    number += 1
+    # if number == 33:
+    #     break
+else:
+    print('else - end')
+print('end')
---
+++ Lesson 2.1.2. Форматирование строк.py
@@ -0,0 +1,22 @@
+name = 'Leo'
+age = 30
+# 1. конкатенация
+hello_str = 'Привет, ' + name + ' тебе ' + str(age) + ' лет'
+print(hello_str)
+# 2. %
+hello_str = 'Привет %s тебе %d лет'%(name, age)
+print(hello_str)
+# 3. format
+hello_str = 'Привет {} тебе {} лет'.format(name, age)
+print(hello_str)
+
+#задание
+top5 = 'Первые 5 мест на соревнованиях: 1. иванов 2. петров 3. сидоров 4. орлов 5. соколов'
+# Поздравляем 1. ИВАНОВ 2. ПЕТРОВ 3. СИДОРОВ с успехом!
+start = top5.find('1')
+end = top5.find('4')
+
+top3 = top5[start: end]
+
+result = 'Поздравляем {} с успехом!'.format(top3.upper() )
+print(result)
